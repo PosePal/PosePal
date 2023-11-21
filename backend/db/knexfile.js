e1b1b8fa5e32b3902c2e2af141c055c82b22dc5e
hanzili <hanzi.li@mail.mcgill.ca>
2023-11-21 13:11:55 -0500

    chore: production config

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -3,7 +3,16 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      // delete dev.sqlite4 and change this to './dev.sqlite3' before npm run migrate
+      // delete dev.sqlite3 and change this to './dev.sqlite3' before npm run migrate
+      // change this to './db/dev.sqlite3' before npm start
+      filename: "./db/dev.sqlite3",
+    },
+    useNullAsDefault: true,
+  },
+  production: {
+    client: "sqlite3",
+    connection: {
+      // delete dev.sqlite3 and change this to './dev.sqlite3' before npm run migrate
       // change this to './db/dev.sqlite3' before npm start
       filename: "./db/dev.sqlite3",
     },
